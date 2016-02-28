c978dfc60f5ba35adba8e46d7ed29b62a2248045
Alex Nazarenko <olexandr.nazarenko@gmail.com>
2016-02-28 18:15:28 +0200

    added setting

---
+++ src/AppBundle/Controller/SettingController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\SettingType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SettingController extends Controller
+{
+    /**
+     * @Route("/setting", name="_setting_page")
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $oldPassword = $user->getPassword();
+        $user->setPassword('');
+
+        $form = $this->createSettingForm($user, 'setting-form');
+
+        $form->add('save', 'submit', array('attr' => array('class' => 'btn-primary')));
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ($user->getPassword() != '') {
+                $user->setPassword(password_hash($user->getPassword(), PASSWORD_DEFAULT));
+            } else {
+                $user->setPassword($oldPassword);
+            }
+        }
+
+        return $this->render(
+            'AppBundle:Setting:index.html.twig',
+            array(
+                'active' => 'setting',
+                'form' => $form->createView()
+            )
+        );
+    }
+
+    public function createSettingForm(User $user, $formClass)
+    {
+        $form = $this->createForm(
+            new SettingType(),
+            $user,
+            array(
+                'action' => $this->generateUrl('_setting_page'),
+                'method' => 'POST',
+                'attr' => array('class' => $formClass)
+            )
+        );
+
+        return $form;
+    }
+}
